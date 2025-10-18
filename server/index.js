e2075d55b3b236a7e14f3086c46abe26ee08b381
Selven11 <ramspmm@gmail.com>
2025-10-17 21:37:25 -0400

    ...

--- server/index.js
+++ server/index.js
@@ -20,10 +20,6 @@ app.get('/talon-secret', (req, res) => {
   res.sendFile(path.join(__dirname, '../client/pages/talon/talon-secret/talon-secret-page.html'));
 });
 
-// Handle talon-secret subdomain with any path
-app.get('/talon-secret/*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../client/pages/talon/talon-secret/talon-secret-page.html'));
-});
 
 app.post('/api/submit', async (req, res) => {
   try {
@@ -36,15 +32,6 @@ app.post('/api/submit', async (req, res) => {
     // Build env var name dynamically: FLAG1_HASH, FLAG2_HASH, ...
     const envVarName = `FLAG${challenge}_HASH`;
     let flagHash = process.env[envVarName];
-    
-    // Fallback to hardcoded flags if env vars not set
-    if (!flagHash) {
-      const hardcodedFlags = {
-        '1': '$2b$10$kNqcRlTJ9RuxEqtM6LAkk.QZN08oMROSbSl7XG.hubpGNFXwYwkam', // TALON{hidden_in_plain_sight_1337}
-        '3': '$2b$10$08LIlz/aS5ldDqv3gQbmJuEqC9JTeRFEfkikTlO1YgmyYfsT84hWC'  // TALON{chatroom_secrets_revealed_2024}
-      };
-      flagHash = hardcodedFlags[challenge];
-    }
 
     if (!flagHash) {
       return res.status(404).json({ success: false, message: 'Challenge not found' });
