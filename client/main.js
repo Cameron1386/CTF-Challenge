1016ac25c8263095de2a2ffe2fbf986d64f10313
Cameron Yarnall <yarnallcameron818@gmail.com>
2026-01-05 20:13:35 -0500

    made it so first challenge goes to seperate page

--- client/main.js
+++ client/main.js
@@ -151,7 +151,7 @@ function getChallenge1Content() {
         <p>The Golden Talon has been bragging about their secret forum where they discuss their "master plans". They think it's hidden, but we know better.</p>
         <p>Click the link below to visit their subdomain and inspect the page to find their forum link and the flag.</p>
         <p><strong>Hint:</strong> Right-click and "Inspect Element" or press F12 to view the page source.</p>
-        <a class="challenge-link" href="talon-secret/talon-secret-page.html">Secret Forum</a>
+        <a class="challenge-link" href="talon-secret/talon-secret-page.html" target="_blank">Secret Forum</a>
     `;
 }
 
