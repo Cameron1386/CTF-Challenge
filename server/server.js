77e8bb3750abccc5bbb7c900de9e48ec12e5db0a
Cameron Yarnall <yarnallcameron818@gmail.com>
2026-03-16 21:52:14 -0400

    backend

--- server/server.js
+++ server/server.js
@@ -15,7 +15,7 @@ const app = express();
 app.use(express.json());
 
 app.use(cors({
-  origin: [FRONTEND, "https://ctf-challenge-9ylr2wwl6-camerons-projects-fe218799.vercel.app"], // for testing only, allows any frontend to connect
+  origin: ["https://ctf-challenge-nine.vercel.app", FRONTEND], // for testing only, allows any frontend to connect
   methods: ["GET", "POST", "PUT", "DELETE"],
   credentials: true
 }));
